01b40a8aaefca65b7e07235a42c4831249a1aede
Francesco <francesco.savignano96@gmail.com>
2020-10-21 16:18:08 +0200

    ES2 impostato

--- pari_o_dispari/main.js
+++ pari_o_dispari/main.js
@@ -1,12 +1,14 @@
 //Pari o dispari: scrivere una (o più di una) funzione per simulare il gioco di pari e dispari tra l'utente e il computer. L'utente sceglie un numero tra 1 e 5 e sceglie "pari" o "dispari". Il computer genera un secondo numero casuale tra 1 e 5. Per vedere chi dei due vince, si deve fare la somma dei numeri dell'utente e del computer e stabilire se il risultato è pari o dispari. Ad esempio, l'utente inserisce il numero 1 e indica come scelta "pari"; il computer genera casualmente il numero 3. In questo caso ha vinto l'utente (perché 1+3 = 4, che è pari)
 
-
+//INIZIO PARTITA
+console.log('INIZIO PARTITA');
 //variabili utente
 var num1, word1;
 
-//chiedo allutente un numero da 1 a 5
+//chiedo all'utente un numero da 1 a 5
 do {
     num1 = parseInt(prompt('Inserire un numero da 1 a 5: '));
+    console.log('user number: ' + num1);
 
     if (num1 < 1 || num1 > 5 || isNaN(num1)) {
         alert('Inserimento sbagliato...');
@@ -17,9 +19,60 @@ do {
 //richiesta pari o dispari all'utente
 do {
     word1 = prompt('Inserire pari o dispari: ');
+    console.log('user word: ' + word1);
 
     if (word1 != 'pari' && word1 != 'dispari') {
         alert('Inserimento sbagliato...');
     }
 
 } while (word1 != 'pari' && word1 != 'dispari');
+
+//variabili avversario
+var num2;
+
+//generazione numero random tra 1 e 5
+num2 = getRndInteger(1, 5);
+console.log('random number generated: ' + num2);
+
+//somma numero utente e numero avversario
+var sum = num1 + num2;
+console.log('sum: ' + sum);
+
+//CONFRONTO E RISULTATO
+console.log('CONFRONTO E RISULTATO');
+
+//stampa a video il vincitore
+challenge(sum, word1);
+
+//FUNZIONI
+//funzione random
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//funzione isEven
+function isEven(num){
+    if (!(num % 2)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+//funzione challenge
+function challenge(sum, UserWord){
+    //se vince user...
+    if((isEven(sum) && (UserWord == 'pari')) || (!(isEven(sum)) && (UserWord == 'dispari'))){
+        console.log('isEven:' + isEven(sum) + ' ' + 'User:'+ UserWord);
+        console.log('Vince user!');
+    }
+    //se vince avversario...
+    else if ((isEven(sum) && (UserWord == 'dispari')) || (!(isEven(sum)) && (UserWord == 'pari'))) {
+        console.log('isEven:' + isEven(sum) + ' ' + 'User:'+ UserWord);
+        console.log('Vince avversario!');
+    }
+    //errore
+    else{
+        alert('ops.. qualcosa è andato storto..')
+    }
+}
